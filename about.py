eb9cd24432deb8bf9c8053f11c8ed26195596e2c
mariafernandamurillo <mariafernandamurillo>
2023-03-18 11:50:14 -0700

    Class 3 - 108

--- about.py
+++ about.py
@@ -3,5 +3,6 @@ me = {
     "name" : "Fernanda",
     "last_name" : "Murillo",
     "age" : 30,
+    "email" : "mariafernanda.murillo@hotmail.com",
     "hobbies" : ["a", "b", "c"] #a list
 }
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import Flask
 import json 
 
 from about import me #importing my dictionary
+from data import mock_data
 
 #Create an instance of Flask class -> app = Flask() 
 app = Flask(__name__) #Magic variables __name__
@@ -31,6 +32,27 @@ def version():
 def about():
     return json.dumps(me)
 
+#get /api/developer/name
+#return the full name of the developer plus the email
+#eg. Sergio Inzunza -- sinzunza@sdgku.edu
+
+@app.get("/api/developer/name")
+def developer_name():
+    name = me["name"]
+    last_name = me["last_name"]
+    email = me["email"]
+    developer = f"{name} {last_name} -- {email}"
+    return json.dumps(developer)
+    # return json.dumps(f"{me["name"]} {me["last_name"]} -- {me["email"]}")
+
+#An endpoint that retuns the infrmation of my catalog
+@app.get("/api/catalog")
+def get_catalog():
+    return json.dumps(mock_data)
+
+@app.get("/api/products/count")
+def products_count():
+    return json.dumps(len(mock_data))
 
 #Start the server
 app.run(debug=True) #debug = True get details about an error,
